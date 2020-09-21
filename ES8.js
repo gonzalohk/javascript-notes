57683b8f5a2248d60413433036b9834da91e93a3
gonzalohk <goh.acm@gmail.com>
2020-09-21 14:25:52 -0400

    some refactors were added

--- ES8.js
+++ ES8.js
@@ -1,63 +1,51 @@
 //ES8 (Ecmascript 2017)
 //async y await
-const coolSong = (iAmABadass) => {
-	return newPromise((resolve, reject) => {
-		if (iAmABadass === true) {
+const coolSong = () => {
+	return new Promise((resolve, reject) => {
+		if (true) {
 			return setTimeout(() => resolve('Welcome to the jungle'), 2000);
 		}
-		return setTimeout(() => reject('All about that bass'), 2000);
+		return setTimeout(() => reject('T.N.T.'), 5000);
 	})
 };
-const coolSongAsync = async (iAmABadass) => {
+const coolSongAsync = async () => {
 	const cool = 'The coolest song is...';
 	console.log(cool);
 	try {
-		const song = await coolSong(iAmABadass);
+		const song = await coolSong();
 		console.log(song);
 	} catch (error) {
-		console.log(`Aparently something went wrong: The result is ${error}`);
+		console.log(`Aparently something went wrong: ${error}`);
 	}
 };
-
-coolSongAsync(true); // The coolest song is... Welcome to the jungle
+coolSongAsync(); // The coolest song is... Welcome to the jungle
 
 //Object.entries
 const data = {
-	name: 'Manuel',
-	age: 24,
-	rocks: true,
+	name: 'Gonzalo',
+	age: 28,
+	rocks: 'M',
 };
-
 const entries = Object.entries(data);
 console.log(entries); // [ [ 'name', 'Manuel' ], [ 'age', 24 ], [ 'rocks', true ] ]
 
 //Object.values
 const data = {
-	name: 'Manuel',
-	age: 24,
-	rocks: true,
+	name: 'Gonzalo',
+	age: 28,
+	gender: 'M',
 };
 const entries = Object.values(data);
-console.log(entries); // [ 'Manuel', 24, true ]
+console.log(entries); // [ 'Gonzalo', 28, 'M' ]
 
 //PadStart
 const string = 'hi';
-const theWitcher = string.padStart(6, '666666'); 
-console.log(theWitcher); // 'wild hunt'
+const newString = string.padStart(5, 'X'); 
+console.log(newString); // 'XXXhi'
 
 //PadEnd
 const string = 'hi';
-const theWitcher = string.padEnd(9, 'gon');
-console.log(theWitcher); // 'the wild hunt'
+const newString = string.padEnd(5, 'X');
+console.log(newString); // 'hiXXX'
 
-//Si se especifican más o menos caracteres, 
-//el string original se mantiene y el segundo se repite hasta completar 
-//los caracteres o se corta dependiendo del caso
-// Ecmascript 8 también introduce las trailing commas 😃
-
-
-
-var totn_string = 'TechOnTheNet';
-
-console.log(totn_string.padStart(2,'A'));
-console.log(totn_string.padEnd(2,'A'));
+//Trailing commas
